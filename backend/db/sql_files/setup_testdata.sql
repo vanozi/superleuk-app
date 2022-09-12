6a3565d16c28fb3a955c8878a0243ed82a212b60
Wouter van Os <wouter.vanos86@gmail.com>
2022-09-12 18:53:25 +0200

    setup testdata extra rol monteur

--- backend/db/sql_files/setup_testdata.sql
+++ backend/db/sql_files/setup_testdata.sql
@@ -1,11 +1,15 @@
 INSERT INTO users (email, hashed_password, is_active, confirmation)
 VALUES ('admin@admin.com','$2b$12$UKv6whbIteBbIsION9igLef5qOS6yzLn1MczUCST7X6RDn18afzZ2', TRUE, NULL),
-       ('werknemer@werknemer.com', '$2b$12$X6OGY1eXztIH2rYDwyVFO.nmrPYq98kla4JmweOu4N/oMgoe3yaKK', TRUE, NULL);
+       ('werknemer@werknemer.com', '$2b$12$X6OGY1eXztIH2rYDwyVFO.nmrPYq98kla4JmweOu4N/oMgoe3yaKK', TRUE, NULL),
+       ('monteur@monteur.com','$2b$12$XE2/M35H39qI7UnDYH9dieI3byg.3VoA3rB0GuEoByvQUt/xTm0y6', TRUE, NULL);
 
 INSERT INTO roles (name, description)
 VALUES ('admin', 'User met admin rechten'),
-       ('werknemer', 'User met algemene werknemers rechten');
+       ('werknemer', 'User met algemene werknemers rechten'),
+       ('monteur', 'User met monteurs rechten');
 
 INSERT INTO user_roles (users_id, roles_id)
 VALUES (1,1),
-       (2,2);
+       (2,2)
+       (3,2),
+       (3,3);
