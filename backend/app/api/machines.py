a38b208fc771eff67a65aad87b5b949f6e70a0c2
Trashcanpanda <sander.vroege@hotmail.com>
2023-03-24 17:32:52 +0100

    Except statement verbeterd

--- backend/app/api/machines.py
+++ backend/app/api/machines.py
@@ -37,8 +37,7 @@ async def post_machine(
                 created_by=current_active_user.email,
                 last_modified_by=current_active_user.email,
             )
-        except Exception as e:
-            print(e)
+        except:
             raise HTTPException(
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                 detail="Er is een onverwachte fout opgetreden, neem contact op met de beheerder",
