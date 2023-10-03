9811f1fa3e1839c9a723bcac515a596a8c1dae4a
vanozi <wouter.vanos86@gmail.com>
2023-10-03 14:18:13 +0200

    machines endpoint

--- backend/app/api/jdlink.py
+++ backend/app/api/jdlink.py
@@ -20,3 +20,17 @@ async def get_organizations():
                        headers={"Authorization": "Bearer " + jd_credentials.access_token,
                                 'Accept': 'application/vnd.deere.axiom.v3+json'})
     return res.json()
+
+
+@router.get(
+    "/machines",
+    dependencies=[Depends(RoleChecker(["admin"]))],
+)
+async def get_machines():
+    jd_credentials = await JDLink.first()
+    res = requests.get(BASE_URL + "/organizations/10047/machines",
+                       headers={"Authorization": "Bearer " + jd_credentials.access_token,
+                                'Accept': 'application/vnd.deere.axiom.v3+json'})
+    return res.json()
+
+
