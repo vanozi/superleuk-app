0392c8593feb8f76fb1ea7aac4d4a085a331d769
vanozi <wouter.vanos86@gmail.com>
2023-10-18 21:38:18 +0200

    eerste opzet

---
+++ quasar-superleuk/src/components/boerderij-kalender/presentational/PBoerderijKalender.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import FullCalendar from '@fullcalendar/vue3';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import { CalendarOptions } from '@fullcalendar/core';
+import nlLocale from '@fullcalendar/core/locales/nl';
+const calendarOptions: CalendarOptions = {
+  plugins: [dayGridPlugin],
+  initialView: 'dayGridMonth',
+  locale: nlLocale,
+};
+</script>
+<template>
+  <FullCalendar ref="workingHoursCalendar" :options="calendarOptions" />
+</template>
---
+++ quasar-superleuk/src/views/boerderij-kalender/IndexView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import PBoederderijKalender from 'src/components/boerderij-kalender/presentational/PBoerderijKalender.vue';
+</script>
+
+<template>
+  <PBoederderijKalender />
+</template>
