a18fe9392de631afe79745cac67eb34b13bc559e
vanozi <williewellee@gmail.com>
2024-12-20 23:48:00 +0100

    dark mode

--- nuxtui/layouts/default.vue
+++ nuxtui/layouts/default.vue
@@ -1,12 +1,22 @@
 <script setup lang="ts">
 const route = useRoute()
 const links = [
-{
-  id: 'hours',
-  label: 'Uren',
-  to: '/uren',
-  icon: 'i-heroicons-clock',
-}]
+  {
+    id: 'hours',
+    label: 'Uren',
+    to: '/uren',
+    icon: 'i-heroicons-clock',
+  }]
+
+const colorMode = useColorMode()
+const isDark = computed({
+  get() {
+    return colorMode.value === 'dark'
+  },
+  set() {
+    colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+  }
+})
 </script>
 
 <template>
@@ -14,9 +24,18 @@ const links = [
     <UDashboardPanel :width="250" :resizable="{ min: 200, max: 300 }" collapsible>
       <UDashboardNavbar class="!border-transparent" :ui="{ left: 'flex-1' }">
         <template #left>
-          <router-link to="/" class="truncate text-gray-900 dark:text-white font-semibold">
+          <router-link to="/" class="truncate text-gray-900 dark:text-white font-semibold pt-1">
             Gebr. Vroege
           </router-link>
+        </template>
+        <template #right>
+          <ClientOnly>
+            <UButton :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'" color="gray"
+              variant="ghost" aria-label="Theme" @click="isDark = !isDark" />
+            <template #fallback>
+              <div class="w-8 h-8" />
+            </template>
+          </ClientOnly>
         </template>
       </UDashboardNavbar>
 
